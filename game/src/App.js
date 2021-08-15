cf85325f31433f3b8f81aec1f2929a0a0d1b6fa0
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-08-15 11:03:16 +0000

    Added board grid at App.js

--- game/src/App.js
+++ game/src/App.js
@@ -4,6 +4,7 @@ import React, {Component} from 'react';
 const totalBoardRows = 50;
 const totalBoardColumns = 50;
 
+// Set Up Board Status
 const myNewBoardStatus = (cellStatus = ( )=> Math.random() < 0.3) => {
   const grid = [];
   for (let r=0; r< totalBoardRows; r++) {
@@ -13,6 +14,20 @@ const myNewBoardStatus = (cellStatus = ( )=> Math.random() < 0.3) => {
     }
   }
   return grid;
+};
+
+// Set Up Board Grid
+const BoardGrid = ({boardStatus, onToggleCellStatus}) => {
+  const handleClick = (r,c) => onToggleCellStatus(r,c);
+  const tr = [];
+  for (let r=0; r< totalBoardRows; r++) {
+    const td= [];
+    for (let c=0; c < totalBoardColumns; c++) {
+      td.push(<td key={`${r},${c}`} className={boardStatus[r][c] ? 'alive' : 'dead'} onClick={() => handleClick(r,c)} />);
+    }
+    tr.push(<tr key={r}>{td}</tr>);
+  }
+  return <table><tbody>{tr}</tbody></table>;
 };
 
 
